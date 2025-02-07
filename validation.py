1a2851cfeee0f23df78c0ebd44f5403fd1cb27b5
Mohammed Ali Rizvi <mo.rizvi@gmail.com>
2025-02-07 08:34:02 -0600

    removing stale validation file

--- validation.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-from pandasql import sqldf
-
-# Open and read the JSON file
-flexjson = pd.read_json('balancesheet.json',orient='index')
-flexjsondf = pd.DataFrame(flexjson)
-
-print(flexjsondf)
